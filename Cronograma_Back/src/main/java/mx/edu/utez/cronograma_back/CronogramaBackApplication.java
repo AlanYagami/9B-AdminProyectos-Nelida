66353e5372d79a6268a8bef5a99d99d000112ba2
AlanYagami <20223tn046@utez.edu.mx>
2025-06-13 12:43:50 -0600

    feat/implementacion del proyeto en spring

---
+++ Cronograma_Back/src/main/java/mx/edu/utez/cronograma_back/CronogramaBackApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cronograma_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CronogramaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CronogramaBackApplication.class, args);
+    }
+
+}
---
+++ Cronograma_Back/src/test/java/mx/edu/utez/cronograma_back/CronogramaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cronograma_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CronogramaBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
